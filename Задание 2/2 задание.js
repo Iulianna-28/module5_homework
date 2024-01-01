780718346f336fe45381dd5ee9710666660827ea
Iulianna-28 <146312849+Iulianna-28@users.noreply.github.com>
2024-01-01 17:32:06 +0500

    2 задание.js

--- Задание 2/2 задание.js
+++ Задание 2/2 задание.js
@@ -1,4 +1,30 @@
+const x = 1;
+switch (x) {
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    default:
+        console.log('Тип х не определен')
+}
+
+
 const x = NaN;
+switch (x) {
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    default:
+        console.log('Тип х не определен')
+}
+
+
+const x = 1;
 switch (x) {
     case 1:
         console.log(1);
